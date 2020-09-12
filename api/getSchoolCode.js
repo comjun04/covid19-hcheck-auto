e0d2341b321833a7aee2accc2986afa298c4e46a
comjun04 <comjun04@gmail.com>
2020-09-12 12:07:14 +0900

    feat: support all regions

--- api/getSchoolCode.js
+++ api/getSchoolCode.js
@@ -3,9 +3,14 @@ const { URLSearchParams } = require('url')
 const { constant, fetch } = require('../utils')
 
 module.exports = async (school) => {
+  const regionCode = constant.regions[school.region]
+  const schoolLevelCode = constant.schoolLevels[school.level]
+  if (!regionCode) throw new Error('올바르지 않은 지역 이름이 입력되었어요! 다시 확인해주세요!')
+  else if (!schoolLevelCode) throw new Error('올바르지 않은 학교급이 입력되었어요! 다시 확인해주세요!')
+
   const queryParams = new URLSearchParams()
-  queryParams.set('lctnScCode', '06')
-  queryParams.set('schulCrseScCode', 4)
+  queryParams.set('lctnScCode', regionCode)
+  queryParams.set('schulCrseScCode', schoolLevelCode)
   queryParams.set('orgName', school.name)
   queryParams.set('currentPageNo', 1)
 
