f921ddbbb0589092784e10bc1725273dd2390f5a
comjun04 <comjun04@gmail.com>
2020-09-12 12:51:07 +0900

    fix: error when empty school name passed

--- api/getSchoolCode.js
+++ api/getSchoolCode.js
@@ -17,8 +17,8 @@ module.exports = async (school) => {
   const result = await fetch('/school?' + queryParams.toString())
     .then(res => res.json())
     .then(json => {
+      if (!Array.isArray(json.schulList) || json.schulList.length < 1) throw new Error('검색된 학교가 하나도 없어요! 학교 이름을 정확하게 입력해주세요!')
       if (json.schulList.length > 1) throw new Error('너무 많은 학교가 검색되었어요! 학교 이름을 정확하게 입력해주세요!')
-      else if (json.schulList.length < 1) throw new Error('검색된 학교가 하나도 없어요! 학교 이름을 정확하게 입력해주세요!')
       else return json.schulList[0].orgCode
     })
 
